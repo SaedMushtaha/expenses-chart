c8725054d5e1e78e4dbe55260c0cdbd1b08c6272
S.A.E.D <S.A.E.D@DESKTOP-7H26SAC>
2022-07-27 19:47:06 +0300

    use state and submit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'; 
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+import Card from '../UI/Card';
+
+function ExpenseItem(props) {
+
+  const [title, setTitle] = useState(props.title);
+ /* title : current value
+  setTitle : updated value */
+
+  const clickHandler = () => {
+    setTitle('Updated!!');
+  }
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      {/* in the line function
+      <button onClick= {() => {console.log('Clicked!')}}>Change title</button> */}
+
+<button onClick= {clickHandler}>Change title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
