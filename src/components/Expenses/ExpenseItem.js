1a53d99ca59eb758e847d4ebc9275592f6492841
S.A.E.D <S.A.E.D@DESKTOP-7H26SAC>
2022-07-28 21:02:24 +0300

    Add Expenses filter

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,28 +1,17 @@
-import React, {useState} from 'react'; 
+import React from 'react';
+
 import ExpenseDate from './ExpenseDate';
-import './ExpenseItem.css';
 import Card from '../UI/Card';
+import './ExpenseItem.css';
 
-function ExpenseItem(props) {
-
-  const [title, setTitle] = useState(props.title);
- /* title : current value
-  setTitle : updated value */
-
-  const clickHandler = () => {
-    setTitle('Updated!!');
-  }
+const ExpenseItem = (props) => {
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      {/* in the line function
-      <button onClick= {() => {console.log('Clicked!')}}>Change title</button> */}
-
-<button onClick= {clickHandler}>Change title</button>
     </Card>
   );
 }
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
   const [enteredDate, setEnteredDate] = useState('');
@@ -41,15 +41,17 @@ const ExpenseForm = () => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-const expenseData = {
-    title: enteredTitle,
-    amount: enteredAmount, 
-    date: new Date(enteredDate)
-};
-console.log(expenseData);
-setEnteredTitle('');
-setEnteredAmount('');
-setEnteredDate('');
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
@@ -57,7 +59,11 @@ setEnteredDate('');
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
+          <input
+            type='text'
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
